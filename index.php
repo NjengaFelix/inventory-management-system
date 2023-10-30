ea3dbbea9a8d2631feef3488261c6cf656c01806
NjengaFelix <felixnjunge@rocketmail.com>
2023-10-30 22:07:53 +0300

    add fetch_assoc

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
+<?php
+//include once db.php
+include_once "db.php";
+//fetch data from table products
+$sql = "SELECT * FROM products";
+$result = mysqli_query($conn, $sql);
+//check if there are any records in the table
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventory Management System</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
 </head>
-
-<body style="background-color: #a0c3cf;">
-    <nav class="nav-welcome">
-        <ul>
-            <li><a class="primary-button" href="sign_up.php">Sign up</a></li>
-    </nav>
-    <div class="hero">
-        <h1>Managerizer system</h1>
-        <p>An inventory management system</p>
-        <form action="login.php" method="get">
-            <button class="primary-button">Login</button>
-        </form>
-    </div>
+<body>
+    <h1>Inventory management system</h1>
+    <table>
+        <tr>
+            <th>name</th>
+            <th>price</th>
+            <th>quantity</th>
+        </tr>
+            <?php 
+            if(mysqli_num_rows($result)>0){
+                while($row = mysqli_fetch_assoc($result)){
+                    echo "<tr><td>".$row['name']."</td><td>".$row['price']."</td><td>".$row['quantity']."</td></tr>";
+                }
+            }
+            ?>
+    </table>
 </body>
-
 </html>
